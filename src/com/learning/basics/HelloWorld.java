ea14b6edce0c724fd056e422b586d13bff89bd63
boparai133 <boparai.aman133@gmail.com>
2024-11-20 20:05:03 -0500

    Project Created

---
+++ src/com/learning/basics/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.learning.basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module BusyQABatch4 {
+}
